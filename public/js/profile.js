662ac1d7bb4bc19604e261c238f681d870f2560f
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-23 21:48:10 -0600

    got delete working seperately from edit button on posts

--- public/js/profile.js
+++ public/js/profile.js
@@ -40,10 +40,35 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const editButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'GET',
+    });
+
+    if (response.ok) {
+      response.json().then((post) => {
+        document.querySelector('#project-name').value = post.title;
+        document.querySelector('#project-desc').value = post.body;
+        document.location.replace('/profile');
+        console.log(post);
+      });
+    } else {
+      alert('Failed to find post post');
+    }
+  }
+};
+
 document
   .querySelector('.new-project-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('#delete')
   .addEventListener('click', delButtonHandler);
+
+  document
+  .querySelector('#edit')
+  .addEventListener('click', editButtonHandler);
