e966484720e92fdeabeb894ea378c28f41e0cb2b
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-23 22:33:29 -0600

    un-broke delete button, fixed user sign-up with userNAME

--- public/js/comment.js
+++ public/js/comment.js
@@ -24,21 +24,21 @@ const newCommentHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/post/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/api/post/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete comment');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to delete comment');
+//     }
+//   }
+// };
 
 document
   .querySelector('#make-comment')
